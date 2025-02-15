7bb636b7ca3fd90c0ce8b66df662738fbcc222f2
amrutadigvijay <amrutasubha@gmail.com>
2025-02-15 13:08:56 +0530

    Done for 15/2

---
+++ URLShortenerMicroservice/Model/UrlMapping.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace URLShortenerMicroservice.Model
+{
+    public class UrlMapping
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string shortUrl { get; set; } = string.Empty;
+        [Required]
+        public string longUrl { get; set; } = string.Empty;
+
+    }
+}
---
+++ URLShortenerMicroservice/Service/URLShortnerService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using URLShortenerMicroservice.Model;
+
+namespace URLShortenerMicroservice.Service
+{
+    public class URLShortnerService : IUrlShortnerService
+    {
+        private const string Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+       
+        private Random _random=new Random();
+        Task<string?> GetOriginalUrlAsync(string shortCode)
+        {
+            throw new NotImplementedException();
+        }
+        Task<string> ShortenUrlAsync(string originalURL)
+        {
+
+            //generate shortcode
+
+            var shortcode = GenerateShortCode();
+            // add prefix if needed
+            var shorturl = "newgen.ly" + shortcode;
+
+            var mapping = new UrlMapping();
+            mapping.shortUrl = shorturl;
+            mapping.longUrl = originalURL;
+
+            
+            throw new NotSupportedException();
+        }
+
+        private string GenerateShortCode(int length = 6)
+        {
+            return new string(Enumerable.Repeat(Alphabet, length).Select(s => s[_random.Next(s.Length)]).ToArray());
+        }
+    }
+}
