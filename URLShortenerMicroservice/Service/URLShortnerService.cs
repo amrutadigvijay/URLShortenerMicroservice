01a9840b92772ef5e157e9907ac1e9741110be74
amrutadigvijay <amrutasubha@gmail.com>
2025-02-16 13:26:16 +0530

    done for today

--- URLShortenerMicroservice/Service/URLShortnerService.cs
+++ URLShortenerMicroservice/Service/URLShortnerService.cs
@@ -1,18 +1,23 @@
 ﻿using System;
+using URLShortenerMicroservice.Data;
 using URLShortenerMicroservice.Model;
 
 namespace URLShortenerMicroservice.Service
 {
     public class URLShortnerService : IUrlShortnerService
     {
+        private UrlShortnerContext _context;
+
         private const string Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
        
         private Random _random=new Random();
-        Task<string?> GetOriginalUrlAsync(string shortCode)
+      public async Task<string?> GetOriginalUrlAsync(string shortUrl)
         {
-            throw new NotImplementedException();
+            var response=   await _context.urlMappings.FirstOrDefaultAsync(s=> s.shortUrl==shortUrl);
+            return response.longUrl;
+            
         }
-        Task<string> ShortenUrlAsync(string originalURL)
+        public async Task<string> ShortenUrlAsync(string originalUrl)
         {
 
             //generate shortcode
@@ -23,15 +28,27 @@ namespace URLShortenerMicroservice.Service
 
             var mapping = new UrlMapping();
             mapping.shortUrl = shorturl;
-            mapping.longUrl = originalURL;
+            mapping.longUrl = originalUrl;
 
-            
-            throw new NotSupportedException();
+           var response= await _context.urlMappings.AddAsync(mapping);
+
+            await _context.SaveChangesAsync();
+           return response.Entity.shortUrl;
         }
 
         private string GenerateShortCode(int length = 6)
         {
             return new string(Enumerable.Repeat(Alphabet, length).Select(s => s[_random.Next(s.Length)]).ToArray());
+        }
+
+        Task<string> IUrlShortnerService.ShortenUrlAsync(string originalUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<string?> IUrlShortnerService.GetOriginalUrlAsync(string shortCode)
+        {
+            throw new NotImplementedException();
         }
     }
 }
